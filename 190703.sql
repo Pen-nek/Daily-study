3f9cc0a238ec6110c2801ee61fb6797aecec8fb3
Pen-nek <Pen-nek@users.noreply.github.com>
2019-07-05 14:07:16 +0900

    Update (190705)

--- 190703.sql
+++ 190703.sql
@@ -1,3 +1,21 @@
+-- SELECT 구문
+SELECT *
+FROM employees;
+
+
+-- employees 테이블로부터 아이디, 이름, 성을 조회하는 질의문
+SELECT employee_id, first_name, last_name
+FROM employees;
+
+
+-- 주석처리 방법
+/*
+여러줄을
+주석처리
+하는 방법
+*/
+
+
 -- 급여순(오름차순)으로 직원의 이름과 급여를 정렬
 SELECT last_name AS 이름, salary
 FROM employees
