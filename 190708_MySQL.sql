7bf97d0bc5a24822e8467f254b7606757b8de0de
Hyemin_Lee <47183983+Pen-nek@users.noreply.github.com>
2019-07-08 22:39:28 +0900

    Update 190708_MySQL.sql

--- 190708_MySQL.sql
+++ 190708_MySQL.sql
@@ -1,4 +1,24 @@
-/**/
+2. MySQL 접근 구문
+
+1) 현재 데이터베이스 목록 보기
+- 나온 목록 중 sys까지는 system 계정이 관리함
+- world는 샘플 데이터베이스
+
+	show databases;
+
+
+2)사용할 데이터베이스 알려주기
+
+	use world;
+
+
+3) 테이블 확인하기
+
+	show tables;
+
+
+4)  auto_increment : 컬럼에 값을 추가하면 자동으로 1씩 증가하게 함. 기본키 등에 사용.
+
 show databases;
 
 use world;
